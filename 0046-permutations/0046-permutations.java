e60467cb04cdf021ee3c18f7beacee8f2de94999
Kapil Kariya <147917943+kapilkariya@users.noreply.github.com>
2026-07-09 18:55:01 +0530

    Time: 0 ms (100%), Space: 45.7 MB (16.43%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,24 +1,28 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> l=new ArrayList<>();
-        HashSet<Integer> s=new HashSet<>();
-        dfs(s,nums,l,ans);
-        return ans;
+        List<List<Integer>> res = new ArrayList<>();
+        permuteHelper(nums, 0, res);
+        return res;
     }
-    public void dfs(HashSet<Integer> s,int[] nums,List<Integer> l,List<List<Integer>> ans){
-        if(s.size()==nums.length){
-            ans.add(new ArrayList<>(l));
+
+    private void permuteHelper(int[] nums, int i, List<List<Integer>> res) {
+        if (i == nums.length) {
+            List<Integer> permutation = new ArrayList<>();
+            for (int num : nums) permutation.add(num);
+            res.add(permutation);
             return;
         }
-        for(int i=0;i<nums.length;i++){
-            if(!s.contains(nums[i])){
-                l.add(nums[i]);
-                s.add(nums[i]);
-                dfs(s,nums,l,ans);
-                s.remove(nums[i]);
-                l.remove(l.size()-1);
-            }
+
+        for (int j = i; j < nums.length; j++) {
+            swap(nums, i, j);
+            permuteHelper(nums, i + 1, res);
+            swap(nums, i, j); // backtrack
         }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }
